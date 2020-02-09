a0aebb7dfcce9aa10923c7390492e7d51c89d7dd
FrankSalmick <frank@salmick.com>
2020-02-09 01:45:02 -0800

    record.js v1.0

--- record.js
+++ record.js
@@ -5,139 +5,127 @@ const configFilename = "./record-config.txt";
 const textColor = "\x1b[37m";
 // ctrl shift g
 var stopKeybind = [29, 42, 34];
+
 var options = {
-    "mousedown": false,
-    "mouseup": false,
-    "keydown": false,
-    // Broken b/c it will trigger when pressing ctrl + shift + g
-    "keyup": false
+    "mouseclick": true,
+    "keyup": true
 };
 
-io.registerShortcut(stopKeybind, () => {
-    io.stop();
-});
-io.on('mousedown', data => {
-    if (options['mousedown']) {
-        console.log("Mousedown: " + data);
+var mainMenu = {
+    "Show config": () => {
+        console.log(options);
+    },
+    "Reload config": () => {
+        if (fs.existsSync(configFilename)) {
+            var diskConfig = fs.readFileSync("./record-config.txt");
+            diskConfig = JSON.parse(diskConfig);
+            // Make sure the file has the right data
+            var matchesExactly = true;
+            Object.keys(options).forEach(value => {
+                if (diskConfig[value] == undefined) {
+                    console.log("Warning: " + value + " (" + options[value] + ") exists in memory but does not exist in the file.");
+                    matchesExactly = false;
+                }
+            });
+            Object.keys(diskConfig).forEach(value => {
+                if (options[value] == undefined) {
+                    console.log("Warning: " + value + " (" + diskConfig[value] + ") exists on disk but does not exist in memory.");
+                    matchesExactly = false;
+                }
+            });
+            var mergeConfigs = function () {
+                // We could just do options = JSON.parse(diskConfig), 
+                // but instead we iterate over diskConfig so that we don't *lose* items in our config (which will probably cause problems down the road)
+                Object.keys(diskConfig).forEach(value => {
+                    options[value] = diskConfig[value];
+                });
+                console.log("Successfully imported config");
+            }
+            if (matchesExactly) {
+                mergeConfigs();
+            } else {
+                printLogo();
+                printColorLine("There were warnings.");
+                do {
+                    console.log("Config on disk:");
+                    console.log(diskConfig);
+                    console.log("Config in memory:");
+                    console.log(options);
+                    // These are two value arrays, the first item is the function to call and the second item is if the loop should repeat again 
+                    var mergeChoices = {
+                        "Merge configs, favoring values present on disk": [mergeConfigs, false],
+                        "Cancel loading (do not merge, use only values in memory)": [() => {
+                            printError("File load aborted by user.");
+                        }, false]
+                    }
+                    var choice = printMenu(mergeChoices);
+                    mergeChoices[choice][0]();
+                } while (mergeChoices[choice][1]);
+            }
+        } else {
+            console.log("Config does not exist on disk, using default.");
+        }
+    },
+    "Save config to disk": () => {
+        if (fs.existsSync(configFilename)) {
+            fs.renameSync(configFilename, configFilename + "-backup");
+            fs.writeFileSync(configFilename, JSON.stringify(options));
+        }
+        else {
+            fs.writeFileSync(configFilename, JSON.stringify(options));
+        }
+        dirtyData = false;
+        console.log("Saved data to disk.");
+    },
+    "Change option": () => {
+        var option = readline.question("Enter the option you would like to change: ");
+        if (options[option] != undefined) {
+            options[option] = !options[option];
+            console.log("Updated value " + option + ", is now " + options[option]);
+            dirtyData = true;
+        } else {
+            console.log("Value " + option + " does not exist.");
+        }
+    },
+    "Record now": () => {
+        io.registerShortcut(stopKeybind, () => {
+            io.stop();
+            process.exit();
+        });
+        Object.keys(options).forEach(value => {
+            console.log("Binding to " + value);
+            io.on(value, data => {
+                if (options[value]) {
+                    fs.appendFile('./playbackfile.txt', JSON.stringify(data) + "\n", (err) => { console.log(err); });
+                }
+            })
+        });
+        io.start();
+        console.log("Use ctrl+shift+g to stop recording.");
+    },
+    "Quit": () => {
+        console.log("You can also use ctrl+c any time.");
+        process.exit();
     }
-})
-io.on('mouseup', data => {
-    if (options['mouseup']) {
-        console.log("mouseup: " + data);
-    }
-})
-io.on('keydown', data => {
-    if (options['keydown']) {
-        console.log("keydown: " + data);
-    }
-})
-io.on('keyup', data => {
-    if (options['keyup']) {
-        console.log("keyup: " + data);
-    }
-})
+}
 
 function main() {
     // Make the text white (I like it more that way)
     console.log(textColor);
     var dirtyData = false;
-    var menu = {
-        "Show config": () => {
-            console.log(options);
-        },
-        "Reload config": () => {
-            if (fs.existsSync(configFilename)) {
-                var diskConfig = fs.readFileSync("./record-config.txt");
-                diskConfig = JSON.parse(diskConfig);
-                // Make sure the file has the right data
-                var matchesExactly = true;
-                Object.keys(options).forEach(value => {
-                    if (diskConfig[value] == undefined) {
-                        console.log("Warning: " + value + " (" + options[value] + ") exists in memory but does not exist in the file.");
-                        matchesExactly = false;
-                    }
-                });
-                Object.keys(diskConfig).forEach(value => {
-                    if (options[value] == undefined) {
-                        console.log("Warning: " + value + " (" + diskConfig[value] + ") exists on disk but does not exist in memory.");
-                        matchesExactly = false;
-                    }
-                });
-                var mergeConfigs = function () {
-                    // We could just do options = JSON.parse(diskConfig), 
-                    // but instead we iterate over diskConfig so that we don't *lose* items in our config (which will probably cause problems down the road)
-                    Object.keys(diskConfig).forEach(value => {
-                        options[value] = diskConfig[value];
-                    });
-                    console.log("Successfully imported config");
-                }
-                if (matchesExactly) {
-                    mergeConfigs();
-                } else {
-                    printLogo();
-                    printColorLine("There were warnings.");
-                    do {
-                        console.log("Config on disk:");
-                        console.log(diskConfig);
-                        console.log("Config in memory:");
-                        console.log(options);
-                        // These are two value arrays, the first item is the function to call and the second item is if the loop should repeat again 
-                        var mergeChoices = {
-                            "Merge configs, favoring values present on disk": [mergeConfigs, false],
-                            "Cancel loading (do not merge, use only values in memory)": [() => {
-                                printError("File load aborted by user.");
-                            }, false]
-                        }
-                        var choice = printMenu(mergeChoices);
-                        mergeChoices[choice][0]();
-                    } while (mergeChoices[choice][1]);
-                }
-            } else {
-                console.log("Config does not exist on disk, using default.");
-            }
-        },
-        "Save config to disk": () => {
-            if (fs.existsSync(configFilename)) {
-                fs.renameSync(configFilename, configFilename + "-backup");
-                fs.writeFileSync(configFilename, JSON.stringify(options));
-            }
-            else {
-                fs.writeFileSync(configFilename, JSON.stringify(options));
-            }
-            dirtyData = false;
-            console.log("Saved data to disk.");
-        },
-        "Change option": () => {
-            var option = readline.question("Enter the option you would like to change: ");
-            if (options[option] != undefined) {
-                options[option] = !options[option];
-                console.log("Updated value " + option + ", is now " + options[option]);
-                dirtyData = true;
-            } else {
-                console.log("Value " + option + " does not exist.");
-            }
-        },
-        "Record now": () => {
-            io.start();
-            console.log("Use ctrl+shift+g to stop recording.");
-        },
-        "Quit": () => {
-            console.log("You can also use ctrl+c any time.");
-            process.exit();
-        }
-    }
 
-    menu["Reload config"]();
-    menu["Show config"]();
+    mainMenu["Reload config"]();
+    mainMenu["Show config"]();
     console.log("Setup complete!");
-    while (1) {
+    var choice;
+    while (choice != "Record now") {
         printLogo();
         if (dirtyData) {
             printError("Config has not been saved.");
         }
-        var choice = printMenu(menu);
-        menu[choice]();
-    }
+        var choice = printMenu(mainMenu);
+        mainMenu[choice]();
+    } 
 }
 
 function printLogo() {
